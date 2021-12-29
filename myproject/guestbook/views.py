6c6cf9adbec9e073f4569009d1af70d2f3c97ea1
Mike Samuels <msamuels59@gmail.com>
2021-12-28 21:21:49 -0500

    finished assignment

--- myproject/guestbook/views.py
+++ myproject/guestbook/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Comment
 from .forms import CommentForm
@@ -10,8 +10,19 @@ def index(request):
     return render(request, 'guestbook/index.html', context)
 
 def sign(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            new_comment = Comment(name=request.POST['name'], comment=request.POST['comment'])
+            new_comment.save()
+            return redirect('index')
+
+    else:
+        form = CommentForm()
+
+
     form = CommentForm()
 
     context = {'form' : form}
-    
-    return render(request, 'guestbook/sign.html' )
+    return render(request, 'guestbook/sign.html', context)
